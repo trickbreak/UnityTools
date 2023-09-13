80fe995280bf506d316161da4f833803f58bcec3
trickbreak <trickbreak@gmail.com>
2023-09-13 16:11:17 +0900

    코루틴 스케줄러 문서 업데이트 및 네임스페이스 적용

--- Assets/Tools/CoroutineScheduler/README.md
+++ Assets/Tools/CoroutineScheduler/README.md
@@ -1,1 +1,129 @@
 # CoroutineScheduler
+
+## 기본 설명
+- 여러 행위(이후 스케줄이라 명명)가 동시다발로 발생하는 경우, 이를 순차 처리하기 위한 스케줄러입니다.
+- 등록된 순서 혹은 스케줄의 우선순위에 따라서 순차적으로 스케줄이 실행할 수 있습니다.
+
+## 기본 규칙
+1. 우선순위의 숫자가 클수록 먼저 실행됩니다.
+2. 실행 중인 스케줄이 없을 때 스케줄이 등록되면 즉시 실행됩니다.
+3. 실행 중인 스케줄이 없을 때 등록된 스케줄의 우선순위가 명시되지 않았다면 우선순위 값은 0입니다.
+4. 실행 중인 스케줄이 있을 때 등록된 스케줄의 우선순위가 명시되지 않았다면 우선순위 값은 가장 마지막에 있는 스케줄의 우선순위 값과 동일합니다.
+5. 우선순위가 같은 스케줄이 추가되면, 우선순위가 같은 작업 중 가장 뒤에 추가됩니다.
+6. 추가되는 스케줄의 우선순위가 실행 중인 스케줄의 우선순위 보다 높더라도, 실행 중인 스케줄이 끝난 뒤 실행됩니다.
+
+## 사용 예시
+- RankingManager와 EventManager가 동시에 각자의 스케줄을 실행하기 위해서 매초 검사 중입니다.
+- RankingManager와 EventManager에서 동시에 조건을 만족하게 되면 스케줄이 겹치게 됩니다.
+- 스케줄러를 도입하여 등록된 순서 또는 우선순위에 맞춰 순차 처리됩니다.
+```csharp
+using System.Collections;
+using Trickbreak.CoroutineScheduler;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public CoroutineScheduler Scheduler { get; private set; }
+
+    private void Awake()
+    {
+        Scheduler = new CoroutineScheduler(this);
+    }
+}
+
+public class EventManager
+{
+    [SerializeField]
+    private GameManager gameManager;
+
+    /// <summary>
+    /// 1초 마다 이벤트 연출을 보여줄 수 있는지 판단 하고, 할 수 있는 경우 이벤트 연출을 스케줄에 등록 합니다.
+    /// </summary>
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            if (CanEventTimeLinePlay())
+            {
+                AddSchedulerEventTimelinePlay();
+                yield break;
+            }
+            else
+            {
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
+    }
+
+    private bool CanEventTimeLinePlay()
+    {
+        // 이벤트 타임라인을 플레이 할 조건을 만족하면 true, 만족 하지 않으면 false
+        return false;
+    }
+    
+    private void AddSchedulerEventTimelinePlay()
+    {
+        // 다른 곳에서 이미 EventTimelinePlay 스케줄을 등록 한 경우 제거하고 다시 등록 합니다.
+        gameManager.Scheduler.RemoveSchedule(tag: "EventTimelinePlay");
+        gameManager.Scheduler.StartSchedule(EventTimelinePlay, tag: "EventTimelinePlay");
+
+        
+        
+        IEnumerator EventTimelinePlay()
+        {
+            Debug.Log("이벤트 연출 시작");
+            yield return new WaitForSeconds(2.0f);
+            Debug.Log("이벤트 연출 종료");
+        }
+    }
+}
+
+
+
+public class RankingManager
+{
+    [SerializeField]
+    private GameManager gameManager;
+
+    /// <summary>
+    /// 1초 마다 랭킹 집계를 할 수 있는지 판단 하고, 할 수 있는 경우 랭킹 집계를 스케줄에 등록 합니다.
+    /// </summary>
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            if (CanRankingScoreCounting())
+            {
+                AddSchedulerRankingScoreCounting();
+                yield break;
+            }
+            else
+            {
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
+    }
+
+    private bool CanRankingScoreCounting()
+    {
+        // 랭킹 집계를 할 수 있으면 true, 할 수 없으면 false
+        return false;
+    }
+    
+    private void AddSchedulerRankingScoreCounting()
+    {
+        // 다른 곳에서 이미 RankingScoreCounting 스케줄을 등록 한 경우 제거하고 다시 등록 합니다.
+        gameManager.Scheduler.RemoveSchedule(tag: "RankingScoreCounting");
+        gameManager.Scheduler.StartSchedule(work: RankingScoreCounting, tag: "RankingScoreCounting");
+        
+        
+        
+        IEnumerator RankingScoreCounting()
+        {
+            Debug.Log("랭킹 점수 집계 시작");
+            yield return new WaitForSeconds(3.0f);
+            Debug.Log("랭킹 점수 집계 종료");
+        }
+    }
+}
+```
--- Assets/Tools/CoroutineScheduler/Runtime/CoroutineScheduler.cs
+++ Assets/Tools/CoroutineScheduler/Runtime/CoroutineScheduler.cs
@@ -3,148 +3,150 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// 규칙 1. 우선 순위는 숫자가 클수록 먼저 실행 됩니다.
-/// 규칙 2. 우선 순위가 같은 스케줄이 추가 되면, 같은 작업 중 가장 뒤에 추가 됩니다.
-/// 규칙 3. 추가 되는 스케줄의 우선 순위가 실행 중인 스케줄의 우선순위 보다 높더라도, 실행 중인 스케줄이 끝난 뒤 실행 됩니다.
-/// 규칙 4. 실행중인 스케줄이 없을때 우선순위가 없는 작업이 추가 되면 추가된 작업의 우선순위는 0 입니다.
-/// </summary>
-public class CoroutineScheduler
+namespace Trickbreak.CoroutineScheduler
 {
-    private class Schedule
-    {
-        private Func<IEnumerator> work;
-        
-        public string Tag { get; }
-
-        public int Priority { get; }
-        
-
-        
-        public Schedule(string tag, int priority, Func<IEnumerator> work)
-        {
-            Tag = tag;
-            Priority = priority;
-            this.work = work;
-        }
-
-        public IEnumerator Invoke()
-        {
-            if (work == null)
-            {
-                yield break;
-            }
-            
-            yield return work.Invoke();
-        }
-    }
-
-    
-    
-    private MonoBehaviour target; 
-    
-    private Coroutine runningCoroutine = null;
-
-    private List<Schedule> schedules = new();
-    
-    
-    
-    public CoroutineScheduler(MonoBehaviour target)
-    {
-        this.target = target;
-    }
-    
     /// <summary>
-    /// 스케줄을 등록 및 실행 합니다.
+    /// 규칙 1. 우선 순위는 숫자가 클수록 먼저 실행 됩니다.<br/>
+    /// 규칙 2. 우선 순위가 같은 스케줄이 추가 되면, 같은 작업 중 가장 뒤에 추가 됩니다.<br/>
+    /// 규칙 3. 추가 되는 스케줄의 우선 순위가 실행 중인 스케줄의 우선순위 보다 높더라도, 실행 중인 스케줄이 끝난 뒤 실행 됩니다.<br/>
+    /// 규칙 4. 실행중인 스케줄이 없을때 우선순위가 없는 작업이 추가 되면 추가된 작업의 우선순위는 0 입니다.
     /// </summary>
-    /// <param name="work">실행 되는 로직 입니다.</param>
-    /// <param name="tag">삭제시 식별을 위한 값 입니다.</param>
-    /// <param name="priority">스케줄이 등록 되는 우선 순위 입니다. 값을 입력 하지 않으면 가장 마지막에 추가 됩니다.</param>
-    /// <returns></returns>
-    public Coroutine StartSchedule(Func<IEnumerator> work, string tag = "noTags", int? priority = null)
+    public class CoroutineScheduler
     {
-        int priorityInt = PriorityConvertToInt(priority);
-
-        Schedule newSchedule = new Schedule(tag, priorityInt, work);
-        
-        AddSchedule(newSchedule);
-        
-        TrySchedulesStart();
-
-        return runningCoroutine;
-
-
-
-        // 우선순위 값이 없는 경우 스케줄의 가장 마지막 우선순위를 부여 합니다.
-        int PriorityConvertToInt(int? value)
+        private class Schedule
         {
-            if (value != null)
-            {
-                return value.Value;
-            }
-                
-            if (schedules.Count > 0)
-            {
-                return schedules[^1].Priority;
-            }
-                
-            return 0;
-        }
-        
-        // 스케줄의 우선순위에 맞춰 리스트에 삽입 합니다.
-        void AddSchedule(Schedule schedule)
-        {
-            // index 0 은 실행중인 스케줄이므로 실행중인 스케줄을 제외 하고 우선 순위를 비교해서 삽입 합니다.
-            for (int i = 1; i < schedules.Count; i++)
-            {
-                if (schedule.Priority > schedules[i].Priority)
-                {
-                    schedules.Insert(i, schedule);
-                    return;
-                } 
-            }
+            private Func<IEnumerator> work;
             
-            schedules.Add(schedule);
-        }
-        
-        // 코루틴이 실행중이 아닌 경우 실행 합니다.
-        void TrySchedulesStart()
-        {
-            if (runningCoroutine == null)
+            public string Tag { get; }
+
+            public int Priority { get; }
+            
+
+            
+            public Schedule(string tag, int priority, Func<IEnumerator> work)
             {
-                runningCoroutine = target.StartCoroutine(SchedulesRun());
+                Tag = tag;
+                Priority = priority;
+                this.work = work;
             }
-        
-            IEnumerator SchedulesRun()
+
+            public IEnumerator Invoke()
             {
-                while (schedules.Count > 0)
+                if (work == null)
                 {
-                    yield return schedules[0].Invoke();
-                    schedules.RemoveAt(0);            
+                    yield break;
                 }
-
-                runningCoroutine = null;
-            }        
-        }
-    }
-
-    /// <summary>
-    /// 실행중이 아닌 스케줄을 제거 할 수 있습니다.
-    /// </summary>
-    public void RemoveSchedule(string tag)
-    {
-        // index 0 은 실행중인 스케줄이라 제외
-        int index = 1;
-        
-        while (index < schedules.Count)
-        {
-            if (schedules[index].Tag == tag)
-            {
-                schedules.RemoveAt(index);
+                
+                yield return work.Invoke();
             }
-            else
+        }
+
+        
+        
+        private MonoBehaviour target; 
+        
+        private Coroutine runningCoroutine = null;
+
+        private List<Schedule> schedules = new();
+        
+        
+        
+        public CoroutineScheduler(MonoBehaviour target)
+        {
+            this.target = target;
+        }
+        
+        /// <summary>
+        /// 스케줄을 등록 및 실행 합니다.
+        /// </summary>
+        /// <param name="work">실행 되는 로직 입니다.</param>
+        /// <param name="tag">삭제시 식별을 위한 값 입니다.</param>
+        /// <param name="priority">스케줄이 등록 되는 우선 순위 입니다. 값을 입력 하지 않으면 가장 마지막에 추가 됩니다.</param>
+        public Coroutine StartSchedule(Func<IEnumerator> work, string tag = "noTags", int? priority = null)
+        {
+            int priorityInt = PriorityConvertToInt(priority);
+
+            Schedule newSchedule = new Schedule(tag, priorityInt, work);
+            
+            AddSchedule(newSchedule);
+            
+            TrySchedulesStart();
+
+            return runningCoroutine;
+
+
+
+            // 우선순위 값이 없는 경우 스케줄의 가장 마지막 우선순위를 부여 합니다.
+            int PriorityConvertToInt(int? value)
             {
-                index++;
+                if (value != null)
+                {
+                    return value.Value;
+                }
+                    
+                if (schedules.Count > 0)
+                {
+                    return schedules[^1].Priority;
+                }
+                    
+                return 0;
+            }
+            
+            // 스케줄의 우선순위에 맞춰 리스트에 삽입 합니다.
+            void AddSchedule(Schedule schedule)
+            {
+                // index 0 은 실행중인 스케줄이므로 실행중인 스케줄을 제외 하고 우선 순위를 비교해서 삽입 합니다.
+                for (int i = 1; i < schedules.Count; i++)
+                {
+                    if (schedule.Priority > schedules[i].Priority)
+                    {
+                        schedules.Insert(i, schedule);
+                        return;
+                    } 
+                }
+                
+                schedules.Add(schedule);
+            }
+            
+            // 코루틴이 실행중이 아닌 경우 실행 합니다.
+            void TrySchedulesStart()
+            {
+                if (runningCoroutine == null)
+                {
+                    runningCoroutine = target.StartCoroutine(SchedulesRun());
+                }
+            
+                IEnumerator SchedulesRun()
+                {
+                    while (schedules.Count > 0)
+                    {
+                        yield return schedules[0].Invoke();
+                        schedules.RemoveAt(0);            
+                    }
+
+                    runningCoroutine = null;
+                }        
+            }
+        }
+
+        /// <summary>
+        /// 실행중이 아닌 스케줄을 제거 할 수 있습니다.
+        /// </summary>
+        public void RemoveSchedule(string tag)
+        {
+            // index 0 은 실행중인 스케줄이라 제외
+            int index = 1;
+            
+            while (index < schedules.Count)
+            {
+                if (schedules[index].Tag == tag)
+                {
+                    schedules.RemoveAt(index);
+                }
+                else
+                {
+                    index++;
+                }
             }
         }
     }
