20da8151ef4e5e0cc724f171d00949f33960feb8
trickbreak <trickbreak@gmail.com>
2023-09-14 16:07:00 +0900

    오브젝트 풀 Get 함수 설명문 업데이트

--- Assets/Tools/AddressableUtility/Runtime/Pools/AddressablePools.cs
+++ Assets/Tools/AddressableUtility/Runtime/Pools/AddressablePools.cs
@@ -36,7 +36,11 @@ namespace Trickbreak.AddressableUtility
         /// 풀링 타겟 오브젝트를 가져 옵니다.
         /// </summary>
         /// <param name="addressableName">풀링 타겟의 어트레서블 키값 입니다.</param>
-        /// <param name="position">풀링 타겟이 활성화(Active - True) 되기 직전에 월드 좌표를 세팅할 수 있습니다.</param>
+        /// <param name="position">풀링 타겟이 활성화(Active - True) 되기 직전에 월드 좌표를 세팅할 수 있습니다.
+        /// <remarks>[주의사항]<br/> 풀링 타겟이 처음 만들어질때(Create) 원본 프리팹의 상태에 따라 gameObject가 켜져있는 상태로 생성될 수 있습니다.<br/>
+        /// 리지드바디가 붙어 있는 오브젝트처럼 꺼져 있는 상태에서 풀링 타겟의 위치를 세팅하고 싶으면 원본 프리펩의 활성상태를 false로 설정해야 합니다.<br/>
+        /// 생성 이후 재활용시에는 Release 처리에서 활성상태를 false 처리하기 때문에 상관 없습니다.</remarks>
+        /// </param>
         /// <param name="parent">풀링 타겟이 활성화(Active - True) 되기 직전에 부모를 세팅할 수 있습니다.</param>
         public PoolingTarget Get(string addressableName, Vector3? position = null, Transform parent = null)
         {
