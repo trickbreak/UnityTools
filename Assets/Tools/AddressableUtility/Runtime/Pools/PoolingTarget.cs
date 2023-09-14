d1c713c2d05591e6994c87658da8ba3f1896e27f
trickbreak <trickbreak@gmail.com>
2023-09-14 22:15:26 +0900

    오브젝트 풀 - 풀링 타겟에 Target을 바로 참조할 수 있는 옵션값 추가

--- Assets/Tools/AddressableUtility/Runtime/Pools/PoolingTarget.cs
+++ Assets/Tools/AddressableUtility/Runtime/Pools/PoolingTarget.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,13 +7,19 @@ namespace Trickbreak.AddressableUtility
     {
         private AddressablePool parentPool;
 
+        [Header("Option")]
+        [Tooltip("참조 하고 싶은 컴포넌트를 하나 추가할 수 있습니다. GetComponent로 컴포넌트를 가져오지 않기 위해 사용할 수 있습니다. (필수X, 옵션O)")]
+        [SerializeField]
+        private MonoBehaviour target;
 
         [Header("Events")]
         [Tooltip("Get 처리시 오브젝트가 활성화(Active - True)되기 직전에 호출되는 이벤트 입니다.")]
-        public UnityEvent OnGetUnityEvent; 
+        [SerializeField]
+        private UnityEvent onGetUnityEvent; 
         
         [Tooltip("Release 처리시 오브젝트가 비활성화(Active - False)되고 바로 호출되는 이벤트 입니다.")]
-        public UnityEvent OnReleaseUnityEvent;
+        [SerializeField]
+        private UnityEvent onReleaseUnityEvent;
         
         
         
@@ -25,12 +30,12 @@ namespace Trickbreak.AddressableUtility
 
         internal void OnGet()
         {
-            OnGetUnityEvent?.Invoke();
+            onGetUnityEvent?.Invoke();
         }
 
         internal void OnRelease()
         {
-            OnReleaseUnityEvent?.Invoke();
+            onReleaseUnityEvent?.Invoke();
         }
 
 
@@ -38,6 +43,15 @@ namespace Trickbreak.AddressableUtility
         public void Release()
         {
             parentPool.Pool.Release(this);
+        }
+
+        /// <summary>
+        /// 옵션으로 Target을 지정한 경우 Target을 가져올 수 있습니다.<br/>
+        /// GetComponent를 사용하지 않고 타겟에 접근할 수 있습니다.
+        /// </summary>
+        public T GetTarget<T>() where T : MonoBehaviour
+        {
+            return target as T;
         }
     }    
 }
