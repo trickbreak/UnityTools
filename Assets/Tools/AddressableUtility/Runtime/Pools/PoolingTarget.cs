e967caefdf80d621e8a16cae84d2db87b4a55e69
trickbreak <trickbreak@gmail.com>
2023-09-15 22:40:13 +0900

    오브젝트풀 타겟의 옵션 값으로 컴포넌트 까지 허용

--- Assets/Tools/AddressableUtility/Runtime/Pools/PoolingTarget.cs
+++ Assets/Tools/AddressableUtility/Runtime/Pools/PoolingTarget.cs
@@ -10,7 +10,7 @@ namespace Trickbreak.AddressableUtility
         [Header("Option")]
         [Tooltip("참조 하고 싶은 컴포넌트를 하나 추가할 수 있습니다. GetComponent로 컴포넌트를 가져오지 않기 위해 사용할 수 있습니다. (필수X, 옵션O)")]
         [SerializeField]
-        private MonoBehaviour target;
+        private Component target;
 
         [Header("Events")]
         [Tooltip("Get 처리시 오브젝트가 활성화(Active - True)되기 직전에 호출되는 이벤트 입니다.")]
@@ -49,7 +49,7 @@ namespace Trickbreak.AddressableUtility
         /// 옵션으로 Target을 지정한 경우 Target을 가져올 수 있습니다.<br/>
         /// GetComponent를 사용하지 않고 타겟에 접근할 수 있습니다.
         /// </summary>
-        public T GetTarget<T>() where T : MonoBehaviour
+        public T GetTarget<T>() where T : Component
         {
             return target as T;
         }
